0754ffbfeb2c8c489227963eba83233bfd7104fa
Sam Ellis <sellis@trinitytg.com>
2017-02-17 09:30:28 -0800

    Initial commit

---
+++ WebApp/src/AVP/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using IdentityServer4.Models;
+
+namespace AVP
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //setup identity server
+            services.AddIdentityServer()
+            .AddInMemoryClients(new List<Client>())
+            .AddInMemoryIdentityResources(new List<IdentityResource>())
+            .AddInMemoryApiResources(new List<ApiResource>())
+            //.AddInMemoryUsers(new List<InMemoryUser>())
+            .AddTemporarySigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            //add identity server
+            app.UseIdentityServer();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
