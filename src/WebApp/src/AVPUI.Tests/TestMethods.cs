257e50784d00f4242377c4683a603628b02272ec
MatthewLoller <Matthewloller@gmail.com>
2017-03-03 12:09:04 -0800

    Added UI Tests Project

---
+++ src/WebApp/src/AVPUI.Tests/TestMethods.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+using System.Windows.Automation;
+
+namespace AVPUI.Tests
+{
+    public class TestMethods
+    {
+        public void EnterText(UITestControl parent, string id, string value)
+        {
+            var edit = new HtmlEdit(parent);
+            edit.SearchProperties.Add(HtmlEdit.PropertyNames.Id, id);
+            edit.Text = value;
+
+        }
+
+        public void ClickRandomCheckBox(UITestControl parent, string value1, string value2, string value3)
+        {
+            var checkBox1 = new HtmlCheckBox(parent);
+            var checkBox2 = new HtmlCheckBox(parent);
+            var checkBox3 = new HtmlCheckBox(parent);
+            Random rand = new Random();
+            int checkNum = rand.Next(1, 3);
+            switch (checkNum)
+            {
+                case '1':
+                    checkBox1.SearchProperties.Add(HtmlEdit.PropertyNames.Id, value1);
+                    Mouse.Click(checkBox1);
+                    break;
+                case '2':
+                    checkBox1.SearchProperties.Add(HtmlEdit.PropertyNames.Id, value2);
+                    Mouse.Click(checkBox1);
+                    break;
+                case '3':
+                    checkBox1.SearchProperties.Add(HtmlEdit.PropertyNames.Id, value3);
+                    Mouse.Click(checkBox1);
+                    break;
+                default:
+                    checkBox1.SearchProperties.Add(HtmlEdit.PropertyNames.Id, value1);
+                    checkBox2.SearchProperties.Add(HtmlEdit.PropertyNames.Id, value2);
+                    checkBox3.SearchProperties.Add(HtmlEdit.PropertyNames.Id, value3);
+                    Mouse.Click(checkBox1);
+                    Mouse.Click(checkBox2);
+                    Mouse.Click(checkBox3);
+                    break;
+            }
+        }
+
+        public void ClickCheckBox(UITestControl parent, string value1)
+        {
+            var checkBox1 = new HtmlCheckBox(parent);
+            checkBox1.SearchProperties.Add(HtmlEdit.PropertyNames.Id, value1);
+            Mouse.Click(checkBox1);           
+        }
+
+        public void ClickButton(UITestControl parent, string value)
+        {
+            //var button = new HtmlInputButton(parent);
+            var button = new HtmlButton(parent);
+            button.SearchProperties.Add(HtmlButton.PropertyNames.Id, value);
+            Mouse.Click(button);
+
+        }
+
+        public void ClickButtonVal(UITestControl parent, string value)
+        {
+            //var button = new HtmlInputButton(parent);
+            var button = new HtmlButton(parent);
+            button.SearchProperties.Add(HtmlButton.PropertyNames.ValueAttribute, value);
+            Mouse.Click(button);
+
+        }
+
+        public void ClickDropDownList(UITestControl parent, string value)
+        {
+            var button = new HtmlComboBox(parent);
+            button.SearchProperties.Add(HtmlComboBox.PropertyNames.Id, value);
+            Mouse.Click(button);
+        }
+
+        public void ClickDropDownListElement(UITestControl parent, string value)
+        {
+            HtmlListItem button = new HtmlListItem(parent);
+            button.SearchProperties.Add(HtmlListItem.PropertyNames.ValueAttribute, value);
+            button.Select();
+
+
+            //HtmlListItem html_listItem = new HtmlListItem(HtmlComboBox);
+            //html_listItem.SearchProperties.Add(HtmlListItem.PropertyNames.ValueAttribute, str_Value);
+            //html_listItem.Select();
+        }
+
+        public void ClickLink(UITestControl parent, string innerText)
+        {
+            var link = new HtmlHyperlink(parent);
+            link.SearchProperties.Add(HtmlHyperlink.PropertyNames.InnerText, innerText);
+            Mouse.Click(link);
+        }
+
+        public void ClickHeader(UITestControl parent, string value)
+        {
+            var link = new HtmlHyperlink(parent);
+            link.SearchProperties.Add(HtmlHyperlink.PropertyNames.ValueAttribute, value);
+            Mouse.Click(link);
+        }
+
+        public void ClickLabel(UITestControl parent, string value)
+        {
+            var link = new HtmlLabel(parent);
+            link.SearchProperties.Add(HtmlHyperlink.PropertyNames.Id, value);
+
+            Mouse.Click(link);
+        }
+
+        public void ClickHead(UITestControl parent, string value)
+        {
+            var link = new HtmlListItem(parent);
+            link.SearchProperties.Add(HtmlListItem.PropertyNames.Id, value);
+
+            Mouse.Click(link);
+        }
+
+        public void ClickableLabel(UITestControl parent, string value)
+        {
+            var link = new HtmlAreaHyperlink(parent);
+            link.SearchProperties.Add(HtmlHyperlink.PropertyNames.Id, value);
+            Mouse.Click(link);
+        }
+    }
+}
---
+++ src/WebApp/src/AVPUI.Tests/UITestMethods.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+
+
+namespace AVPUI.Tests
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class UITestMethods : TestMethods
+    {
+        public UITestMethods()
+        {
+        }
+
+        [TestMethod]
+        public void UserLoginUITest()
+        {
+            TestMethods test = new TestMethods();
+            var broswer = BrowserWindow.Launch("http://localhost:31419/Thunderstruck.html");
+            test.EnterText(broswer, "username", "WebTest");
+            test.EnterText(broswer, "password", "password");
+            test.ClickButton(broswer, "loginBtn");
+            test.ClickButton(broswer, "saveUserInfoBtn");
+            test.ClickLabel(broswer, "savedUserInfoLabel");
+            test.ClickLabel(broswer, "logoutBtn");
+            test.ClickButton(broswer, "loginBtn");
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+
+        [TestMethod]
+        public void UpdateUserInfoUITest()
+        {
+            TestMethods test = new TestMethods();
+            var broswer = BrowserWindow.Launch("http://localhost:31419/Thunderstruck.html");
+            test.EnterText(broswer, "username", "WebTest");
+            test.EnterText(broswer, "password", "password");
+            test.ClickButton(broswer, "loginBtn");
+            test.EnterText(broswer, "resetPassword", "password");
+            test.EnterText(broswer, "confirmPassword", "password");
+            test.EnterText(broswer, "userDisplayName", "WebTest");
+            test.EnterText(broswer, "streetAddress", "2015 J Street");
+            test.EnterText(broswer, "city", "Sacramento");
+            test.ClickDropDownList(broswer, "selectState");
+            test.ClickDropDownListElement(broswer, "CA");
+            test.EnterText(broswer, "zipCode", "95819");
+            test.EnterText(broswer, "emailAddress", "mloller@trinitytg.com");
+            test.EnterText(broswer, "phoneNumber", "9167984676");
+            test.ClickButton(broswer, "saveUserInfoBtn");
+            test.ClickLabel(broswer, "savedUserInfoLabel");
+            test.ClickLabel(broswer, "logoutBtn");
+            test.ClickButton(broswer, "loginBtn");
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+
+        [TestMethod]
+        public void UpdateUserProfileRandomOptInOutUITest()
+        {
+            TestMethods test = new TestMethods();
+            var broswer = BrowserWindow.Launch("http://localhost:31419/Thunderstruck.html");
+            test.EnterText(broswer, "username", "WebTest");
+            test.EnterText(broswer, "password", "password");
+            test.ClickButton(broswer, "loginBtn");
+            test.EnterText(broswer, "resetPassword", "password");
+            test.EnterText(broswer, "confirmPassword", "password");
+            test.EnterText(broswer, "userDisplayName", "WebTest");
+            test.EnterText(broswer, "streetAddress", "2015 J Street");
+            test.EnterText(broswer, "city", "Sacramento");
+            test.ClickDropDownList(broswer, "selectState");
+            test.ClickDropDownListElement(broswer, "CA");
+            test.EnterText(broswer, "zipCode", "95819");
+            test.EnterText(broswer, "emailAddress", "mloller@trinitytg.com");
+            test.EnterText(broswer, "phoneNumber", "9167984676");
+            test.ClickRandomCheckBox(broswer, "emailNotifications", "pushNotifications", "smsNotification");
+            test.ClickButton(broswer, "saveUserInfoBtn");
+            test.ClickLabel(broswer, "savedUserInfoLabel");
+            test.ClickLabel(broswer, "logoutBtn");
+            test.ClickButton(broswer, "loginBtn");
+        }
+
+        [TestMethod]
+        public void RegisterUserUITest()
+        {
+            TestMethods test = new TestMethods();
+            var broswer = BrowserWindow.Launch("http://localhost:31419/Thunderstruck.html");
+            test.ClickLabel(broswer, "registerLabel2");
+            test.EnterText(broswer, "regUsername", "Test");
+            test.EnterText(broswer, "resetPassword", "Test");
+            test.EnterText(broswer, "confirmPassword", "Test");
+            test.EnterText(broswer, "userDisplayName", "Test");
+            test.ClickButton(broswer, "registerBtn");
+            var broswer2 = BrowserWindow.Launch("http://localhost:31419/Thunderstruck.html");
+            test.EnterText(broswer2, "username", "Test");
+            test.EnterText(broswer2, "password", "Test");
+            test.ClickButton(broswer2, "loginBtn");
+            test.ClickButton(broswer2, "saveUserInfoBtn");
+            test.ClickLabel(broswer2, "logoutBtn");
+            test.ClickButton(broswer2, "loginBtn");
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+
+        [TestMethod]
+        public void OptInOutSMSUITest()
+        {
+            TestMethods test = new TestMethods();
+            var broswer = BrowserWindow.Launch("http://localhost:31419/Thunderstruck.html");
+            test.EnterText(broswer, "username", "WebTest");
+            test.EnterText(broswer, "password", "password");
+            test.ClickButton(broswer, "loginBtn");
+            test.EnterText(broswer, "resetPassword", "password");
+            test.EnterText(broswer, "confirmPassword", "password");
+            test.EnterText(broswer, "userDisplayName", "WebTest");
+            test.EnterText(broswer, "streetAddress", "2015 J Street");
+            test.EnterText(broswer, "city", "Sacramento");
+            test.ClickDropDownList(broswer, "selectState");
+            test.ClickDropDownListElement(broswer, "CA");
+            test.EnterText(broswer, "zipCode", "95819");
+            test.EnterText(broswer, "emailAddress", "mloller@trinitytg.com");
+            test.EnterText(broswer, "phoneNumber", "9167984676");
+            test.ClickCheckBox(broswer, "smsNotification");
+            test.ClickButton(broswer, "saveUserInfoBtn");
+            test.ClickLabel(broswer, "savedUserInfoLabel");
+            test.ClickLabel(broswer, "logoutBtn");
+            test.ClickButton(broswer, "loginBtn");
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+
+        [TestMethod]
+        public void OptInOutPushUITest()
+        {
+            TestMethods test = new TestMethods();
+            var broswer = BrowserWindow.Launch("http://localhost:31419/Thunderstruck.html");
+            test.EnterText(broswer, "username", "WebTest");
+            test.EnterText(broswer, "password", "password");
+            test.ClickButton(broswer, "loginBtn");
+            test.EnterText(broswer, "resetPassword", "password");
+            test.EnterText(broswer, "confirmPassword", "password");
+            test.EnterText(broswer, "userDisplayName", "WebTest");
+            test.EnterText(broswer, "streetAddress", "2015 J Street");
+            test.EnterText(broswer, "city", "Sacramento");
+            test.ClickDropDownList(broswer, "selectState");
+            test.ClickDropDownListElement(broswer, "CA");
+            test.EnterText(broswer, "zipCode", "95819");
+            test.EnterText(broswer, "emailAddress", "mloller@trinitytg.com");
+            test.EnterText(broswer, "phoneNumber", "9167984676");
+            test.ClickCheckBox(broswer, "pushNotifications");
+            test.ClickButton(broswer, "saveUserInfoBtn");
+            test.ClickLabel(broswer, "savedUserInfoLabel");
+            test.ClickLabel(broswer, "logoutBtn");
+            test.ClickButton(broswer, "loginBtn");
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+
+        [TestMethod]
+        public void OptInOutEmailUITest()
+        {
+            TestMethods test = new TestMethods();
+            var broswer = BrowserWindow.Launch("http://localhost:31419/Thunderstruck.html");
+            test.EnterText(broswer, "username", "WebTest");
+            test.EnterText(broswer, "password", "password");
+            test.ClickButton(broswer, "loginBtn");
+            test.EnterText(broswer, "resetPassword", "password");
+            test.EnterText(broswer, "confirmPassword", "password");
+            test.EnterText(broswer, "userDisplayName", "WebTest");
+            test.EnterText(broswer, "streetAddress", "2015 J Street");
+            test.EnterText(broswer, "city", "Sacramento");
+            test.ClickDropDownList(broswer, "selectState");
+            test.ClickDropDownListElement(broswer, "CA");
+            test.EnterText(broswer, "zipCode", "95819");
+            test.EnterText(broswer, "emailAddress", "mloller@trinitytg.com");
+            test.EnterText(broswer, "phoneNumber", "9167984676");
+            test.ClickCheckBox(broswer, "emailNotifications");
+            test.ClickButton(broswer, "saveUserInfoBtn");
+            test.ClickLabel(broswer, "savedUserInfoLabel");
+            test.ClickLabel(broswer, "logoutBtn");
+            test.ClickButton(broswer, "loginBtn");
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+
+        [TestMethod]
+        public void HeaderElementsUITest()
+        {
+            TestMethods test = new TestMethods();
+            var broswer = BrowserWindow.Launch("http://localhost:31419/Thunderstruck.html");
+            test.EnterText(broswer, "username", "Test");
+            test.EnterText(broswer, "password", "Test");
+            test.ClickButton(broswer, "loginBtn");
+            test.ClickLabel(broswer, "dashboard");
+            test.ClickLabel(broswer, "incidents");
+            test.ClickLabel(broswer, "editProfile");
+            test.ClickLabel(broswer, "switchRole");
+            test.ClickButton(broswer, "okBtn");
+            test.ClickLabel(broswer, "logoutBtn");
+            test.ClickButton(broswer, "loginBtn");
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+
+
+
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        private TestContext testContextInstance;
+    }
+}
